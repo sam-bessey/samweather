964d00ff302358fa620a2f9a06a0d1aea3ee6c85
Samuel Bessey <samueljbessey@gmail.com>
2026-04-19 16:14:09 -0400

    add forecasting

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,28 @@ async function fetchWeather(coordinates) {
                     console.log("Main: OK");
                     const mainData = await mainResponse.json();
                     console.log("Main data:", mainData);
-                    return mainData.properties.periods[0].detailedForecast;
+
+                    // Get the hourly data
+                    try {
+                        const hourlyResponse = await fetch(
+                            infoData.properties.forecastHourly,
+                        );
+                        if (hourlyResponse.ok) {
+                            console.log("Hourly: OK");
+                            const hourlyData = await hourlyResponse.json();
+                            console.log("Hourly data:", hourlyData);
+
+                            // Return all the data
+                            return [infoData, mainData, hourlyData];
+                        }
+                    } catch (error) {
+                        console.error(
+                            "Error gettting hourly weather data",
+                            error,
+                        );
+                        return error;
+                    }
                 }
-                
             } catch (error) {
                 console.error("Error gettting main weather data", error);
                 return error;
@@ -64,7 +83,7 @@ export default function App() {
     return (
         <div className="m-0 p-0">
             <TitleBar></TitleBar>
-            <p className="p-2">{JSON.stringify(weatherData, null, 2)}</p>
+            <p className="p-2">{JSON.stringify(weatherData[1].properties.periods[0].detailedForecast, null, 2)}</p>
         </div>
     );
 }
