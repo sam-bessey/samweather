36cff6ab76f029b63020173620ba9bf02abfda8c
Samuel Bessey <samueljbessey@gmail.com>
2026-04-19 15:42:10 -0400

    start of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
 
-export default function App (){
+function TitleBar() {
     return (
-        <>
-        <p>hi</p>
-        </>
-    )
+        <div className="bg-blue-300 w-full m-0 p-3">
+            <p className="text-center">SamWeather</p>
+        </div>
+    );
+}
+
+export default function App() {
+    function fetchWeather(coordinates) {
+        // Coordinates: [lat, long]
+        try {
+            const lat = Number(coordinates[0]).toFixed(4);
+        } catch (error) {
+            console.error("Error getting weather data", error);
+        }
+    }
+    return (
+        <div className="m-0 p-0">
+            <TitleBar></TitleBar>
+            <p className="p-2">text</p>
+        </div>
+    );
 }
