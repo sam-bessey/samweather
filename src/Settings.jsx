c18d7a7b6c56437dd7e1b5c5c5851967d7d0c206
sam-bessey <samueljbessey@gmail.com>
2026-09-21 13:32:58 +0000

    git commit seems broken

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -2,5 +2,5 @@ import { motion } from "motion/react";
 import { useState, useEffect, useRef } from "react";
 
 export default function SettingsPage() {
-    return <div>hi</div>;
+    return <div className="w-full h-full backdrop-blur-3xl z-999 absolute">Settings</div>;
 }
